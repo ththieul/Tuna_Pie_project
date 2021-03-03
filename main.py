1072f53979b81bd4332d52fc70d429e00bbbbc4f
ththieul <tibo.thieuleux@gmail.com>
2021-03-03 12:43:42 +0100

    modified main.py with img names

--- main.py
+++ main.py
@@ -16,18 +16,18 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tuna pie EPSI")
 
 # Load images
-RED_SPACE_SHIP = pygame.image.load(os.path.join("assets/img", "pixel_ship_red_small.png"))
-GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets/img", "pixel_ship_green_small.png"))
-BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets/img", "pixel_ship_blue_small.png"))
+RED_SPACE_SHIP = pygame.image.load(os.path.join("assets/img", "pie.png"))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets/img", "pie_2.png"))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets/img", "pie_3.png"))
 
 # Player player
-YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets/img", "pixel_ship_yellow.png"))
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets/img", "handi.png"))
 
 # Lasers
 RED_LASER = pygame.image.load(os.path.join("assets/img", "pixel_laser_red.png"))
 GREEN_LASER = pygame.image.load(os.path.join("assets/img", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets/img", "pixel_laser_blue.png"))
-YELLOW_LASER = pygame.image.load(os.path.join("assets/img", "pixel_laser_yellow.png"))
+YELLOW_LASER = pygame.image.load(os.path.join("assets/img", "poisson.png"))
 
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets/img", "background-black.png")), (WIDTH, HEIGHT))
